3161019118004110d8e0b2e7c8df1d72da5b8ef3
liberty293 <40408116+liberty293@users.noreply.github.com>
2022-02-24 22:21:08 -0800

    added motor movement and fixed open/close door
    Note. THIS HAS NOT BEEN TESTED. PLEASE TEST BEFORE CONFIRMING THIS CODE IS CORRECT

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,26 @@
 #define REDLED 23
 #define BLUELED 22
 #define WAITINGLED 21
-#define IN3 19 //controls right wheel dir
-#define IN1 18 //controls left wheel dir
-#define IN4 17 //controls right wheel dir
-#define IN2 16 //controls left wheel dir
-#define ENA 3 //left wheel PWM
-#define ENB 4 //right wheel PWM
+#define IN3_R 19 //controls right wheel dir
+#define IN1_L 18 //controls left wheel dir
+#define IN4_R 17 //controls right wheel dir
+#define IN2_L 16 //controls left wheel dir
+#define ENA_L 3 //left wheel PWM
+#define ENB_R 4 //right wheel PWM
 #define STEP 14 // for stepper motor
 #define DIR 15 // for stepper motor
 #define TEAMPOTPIN A6
 #define speed_value 200
 //#define GGTIME 250000000
 #define GGTIME 20000000
+
+#define Line_Threshold 2.5 //FILL THIS AND BELOW IN!
+#define Left_Line 1
+#define LeftCenter_Line 2
+#define Center_Line 3
+#define RightCenter_Line 4
+#define Right_Line 5
+
 /*---------------Module Function Prototypes-----------------*/
 //handle state functions
 void handleWaiting(void);
@@ -38,6 +46,7 @@ void closeDoor(void);
 
 //additional useful functions
 int potRead(int pin);
+bool OnLine(int line);
 
 
 
@@ -56,12 +65,12 @@ AccelStepper doorStepper(1, STEP, DIR);
 /*---------------Main Functions----------------*/
 
 void setup() {
-  // pinMode(ENA, OUTPUT);
-  // pinMode(ENB, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+  // pinMode(ENA_L, OUTPUT);
+  // pinMode(ENB_R, OUTPUT);
+  pinMode(IN1_L, OUTPUT);
+  pinMode(IN2_L, OUTPUT);
+  pinMode(IN3_R, OUTPUT);
+  pinMode(IN4_R, OUTPUT);
   pinMode(DIR, OUTPUT);
   pinMode(STEP, OUTPUT);
   pinMode(BLUELED, OUTPUT);
@@ -72,7 +81,6 @@ void setup() {
   GGtimer.begin(handleGG,GGTIME);
   doorStepper.setMaxSpeed(800);
   doorStepper.setSpeed(600);
-
   Serial.begin(9600);
 
 }
@@ -145,50 +153,96 @@ void handleGG(void){ // all motors off, all LEDs off
 }
 
 void driveForward(int speed){//moves robot forward
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, speed);
-  analogWrite(ENB, speed);
+  digitalWrite(IN1_L, HIGH);
+  digitalWrite(IN2_L, LOW);
+  digitalWrite(IN3_R, HIGH);
+  digitalWrite(IN4_R, LOW);
+  analogWrite(ENA_L, speed);
+  analogWrite(ENB_R, speed);
 
 
 }
 
-void driveBackward(void){ // moves robot backward
-
+void driveBackward(int speed){ // moves robot backward
+  digitalWrite(IN2_L, HIGH);
+  digitalWrite(IN1_L, LOW);
+  digitalWrite(IN4_R, HIGH);
+  digitalWrite(IN3_R, LOW);
+  analogWrite(ENA_L, speed);
+  analogWrite(ENB_R, speed);
 }
 
 void stopDriving(void){ // stops driving motors
-
+  analogWrite(ENA_L, 0);
+  analogWrite(ENB_R, 0);
 }
 
 void stopAllMotors(void){ // stops all motors
-
+//I don't know what this is For?
 }
 
-void turnRight(void){ // turns robot right
-
+void turnRight(int speed){ // turns robot right
+  digitalWrite(IN4_R, HIGH);
+  digitalWrite(IN3_R, LOW);
+  digitalWrite(IN1_L, HIGH);
+  digitalWrite(IN2_L, LOW);
+  analogWrite(ENA_L, speed);
+  analogWrite(ENB_R, speed);
 }
 
-void turnLeft(void){ // turns robot left
-
+void turnLeft(int speed){ // turns robot left Right wheel forward; left wheel back
+  digitalWrite(IN3_R, HIGH);
+  digitalWrite(IN4_R, LOW);
+  digitalWrite(IN2_L, HIGH);
+  digitalWrite(IN1_L, LOW);
+  analogWrite(ENA_L, speed);
+  analogWrite(ENB_R, speed);
 }
 
 void lineFollow(void){ // line following
+//if center black move forward and check if other sensor black then at crossroads
+if(OnLine(Center_Line))
+{
+  driveForward(speed_value);
+  if(OnLine(Right_Line))
+    { //make state turning to goal
+    }
+  if(OnLine(Left_Line))
+    { //make state turning to goal
+    }
+}
+//if center white
+else
+{
+//if left black turn right
+  if(OnLine(LeftCenter_Line))
+    turnRight(speed_value);
 
+//if right black turn left
+  if(OnLine(RightCenter_Line))
+    turnLeft(speed_value);
+}
 }
 
 void openDoor(void){
    doorStepper.moveTo(5000);
    doorStepper.setSpeed(600);
+   doorStepper.run();
 
 }
 
 void closeDoor(void){
   doorStepper.moveTo(-5000);
   doorStepper.setSpeed(600);
+  doorStepper.run();
 
+}
+
+bool OnLine(int line){
+  if (digitalRead(line) < Line_Threshold)
+    return true;
+  else
+    return false;
 }
 
 
