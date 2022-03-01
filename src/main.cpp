57d218ee6ad75f0db773b24772db86a483100ed0
liberty293 <40408116+liberty293@users.noreply.github.com>
2022-03-01 12:02:34 -0800

    updated line following

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,36 @@
 /*---------------Libraries-----------------------------*/
 #include <Arduino.h>
+#include <A4988.h>
 #include <SPI.h>
 #include <AccelStepper.h>
 /*---------------Module Defines-----------------------------*/
 #define REDLED 23
 #define BLUELED 22
 #define WAITINGLED 21
-#define IN3_R 19 //controls right wheel dir
-#define IN1_L 18 //controls left wheel dir
-#define IN4_R 17 //controls right wheel dir
-#define IN2_L 16 //controls left wheel dir
-#define ENA_L 3 //left wheel PWM
-#define ENB_R 4 //right wheel PWM
-#define STEP 14 // for stepper motor
-#define DIR 15 // for stepper motor
+#define IN3_R 19 //controls right wheel dir blue
+#define IN1_L 18 //controls left wheel dir green
+#define IN4_R 17 //controls right wheel dir yellow 
+#define IN2_L 16 //controls left wheel dir orange
+#define ENA_L 3 //left wheel PWM black
+#define ENB_R 4 //right wheel PWM white
+#define STEP 14 // for stepper motor orange
+#define DIR 15 // for stepper motor yellow
 #define TEAMPOTPIN A6
-#define speed_value 100
+#define speed_value 300
 #define GGTIME 250000000
 //#define GGTIME 20000000
 
-#define Line_Threshold 2.5 //FILL THIS AND BELOW IN!
-#define Left_Line 1
-#define LeftCenter_Line 2
-#define Center_Line 3
-#define RightCenter_Line 4
-#define Right_Line 5
+#define Line_Threshold 4.0 //FILL THIS AND BELOW IN! 3.5-4.4
+#define Left_Line 10
+#define LeftCenter_Line 9
+#define Center_Line 8
+#define RightCenter_Line 7
+#define Right_Line 6
+
+#define MS1 10
+#define MS2 11
+#define MS3 12
+
 
 /*---------------Module Function Prototypes-----------------*/
 //handle state functions
@@ -38,8 +44,8 @@ void driveForward(int speed);
 void driveBackward(void);
 void stopDriving(void);
 void stopAllMotors(void);
-void turnRight(void);
-void turnLeft(void);
+void turnRight(int speed);
+void turnLeft(int speed);
 void lineFollow(void);
 void openDoor(void);
 void closeDoor(void);
@@ -61,12 +67,13 @@ States_t state;
 int potval;
 int teamcolorval;
 AccelStepper doorStepper(1, STEP, DIR);
+//A4988 stepper(200, DIR, STEP);
 
 /*---------------Main Functions----------------*/
 
 void setup() {
-  // pinMode(ENA_L, OUTPUT);
-  // pinMode(ENB_R, OUTPUT);
+   pinMode(ENA_L, OUTPUT);
+   pinMode(ENB_R, OUTPUT);
   pinMode(IN1_L, OUTPUT);
   pinMode(IN2_L, OUTPUT);
   pinMode(IN3_R, OUTPUT);
@@ -77,31 +84,43 @@ void setup() {
   pinMode(REDLED, OUTPUT);
   pinMode(WAITINGLED, OUTPUT);
   pinMode(TEAMPOTPIN, INPUT);
-  state = WAITING;
+  pinMode(Left_Line,INPUT);
+  pinMode(LeftCenter_Line, INPUT); 
+  pinMode(Center_Line,INPUT) ;
+  pinMode(RightCenter_Line,INPUT) ;
+  pinMode(Right_Line,INPUT) ;
+  state = GO_TO_SHEEP_BLUE;
   GGtimer.begin(handleGG,GGTIME);
-  doorStepper.setMaxSpeed(800);
-  doorStepper.setSpeed(600);
+  doorStepper.setMaxSpeed(1000);
+  doorStepper.setSpeed(1000);
   Serial.begin(9600);
+ // stepper.begin(1, 1);
 
 }
 void loop(){
-  switch (state) {
-    case WAITING:
-      handleWaiting();
-      break;
-    case GO_TO_SHEEP_BLUE:
-      handleGoToBlueSheep();
-      break;
-    case GO_TO_SHEEP_RED:
-      handleGoToRedSheep();
+  closeDoor();
+  //stepper.rotate(360);
+  //Serial.println(analogRead(Left_Line));
+  //lineFollow();
+  // doorStepper.runSpeed();
+  // Serial.println(doorStepper.currentPosition());
+  // switch (state) {
+  //   case WAITING:
+  //     handleWaiting();
+  //     break;
+  //   case GO_TO_SHEEP_BLUE:
+  //     handleGoToBlueSheep();
+  //     break;
+  //   case GO_TO_SHEEP_RED:
+  //     handleGoToRedSheep();
 
-      break;
-    case GG:
-      handleGG();
-      break;
-    default:    // Should never get into an unhandled state
-      Serial.println("What is this I do not even...");
-  }
+  //     break;
+  //   case GG:
+  //     handleGG();
+  //     break;
+  //   default:    // Should never get into an unhandled state
+  //     Serial.println("What is this I do not even...");
+  // }
 }
 
 
@@ -131,7 +150,7 @@ void handleGoToBlueSheep(void){
     digitalWrite(BLUELED,HIGH);
     digitalWrite(WAITINGLED,LOW);
     //driveForward(speed_value);
-    openDoor();
+    closeDoor();
     //INSERT TIMED STOP
 
 }
@@ -157,15 +176,15 @@ void handleGG(void){ // all motors off, all LEDs off
 
 void driveForward(int speed){//moves robot forward
   Serial.println("fwd");
-  digitalWrite(IN1_L, HIGH);
-  digitalWrite(IN2_L, LOW);
+  digitalWrite(IN1_L, LOW);
+  digitalWrite(IN2_L, HIGH);
   digitalWrite(IN3_R, HIGH);
   digitalWrite(IN4_R, LOW);
-  analogWrite(ENA_L, speed);
-  analogWrite(ENB_R, speed);
-  delay(3000);
-  stopDriving();
-  delay(5000);  
+  analogWrite(ENA_L, 600);
+  analogWrite(ENB_R, 600);
+  //delay(3000);
+  //stopDriving();
+  //delay(5000);  
 
 
 }
@@ -203,8 +222,8 @@ void turnRight(int speed){ // turns robot right
 void turnLeft(int speed){ // turns robot left Right wheel forward; left wheel back
   digitalWrite(IN3_R, HIGH);
   digitalWrite(IN4_R, LOW);
-  digitalWrite(IN2_L, HIGH);
-  digitalWrite(IN1_L, LOW);
+  digitalWrite(IN1_L, HIGH);
+  digitalWrite(IN2_L, LOW);
   analogWrite(ENA_L, speed);
   analogWrite(ENB_R, speed);
 }
@@ -235,33 +254,36 @@ else
 }
 
 void openDoor(void){
-  Serial.println("open door");
+  //Serial.println("open door");
    //doorStepper.moveTo(-100);
    //doorStepper.setSpeed(-600); 
    //doorStepper.run();
    
   doorStepper.moveTo(-800); 
   doorStepper.setSpeed(-600); 
+  //doorStepper.run();
   
-  while (doorStepper.currentPosition() != doorStepper.targetPosition()) { 
+  //while (doorStepper.currentPosition() != doorStepper.targetPosition()) { 
     doorStepper.runSpeedToPosition();
-    }
+Serial.println("yo");
+  //  }
 
 }
 
 void closeDoor(void){
-  Serial.println("close door");
+
   doorStepper.moveTo(800); 
   doorStepper.setSpeed(600); 
   
-  while (doorStepper.currentPosition() != doorStepper.targetPosition()) { 
-    doorStepper.runSpeedToPosition();
-    }
+ // while (doorStepper.currentPosition() != doorStepper.targetPosition()) { 
+  doorStepper.runSpeedToPosition();
+      Serial.println(doorStepper.currentPosition());
+   // }
 
 }
 
 bool OnLine(int line){
-  if (digitalRead(line) < Line_Threshold)
+  if (analogRead(line) == 1)
     return true;
   else
     return false;
